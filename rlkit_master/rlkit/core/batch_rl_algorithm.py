932e65425916079be23a290ab367588135066e03
Maria Golitsyna <GolitsynaM@ru.ibm.com>
2020-05-01 11:58:35 +0300

    now pathes whole pathses

--- rlkit_master/rlkit/core/batch_rl_algorithm.py
+++ rlkit_master/rlkit/core/batch_rl_algorithm.py
@@ -61,23 +61,32 @@ class BatchRLAlgorithm(BaseRLAlgorithm, metaclass=abc.ABCMeta):
                 discard_incomplete_paths=True,
             )
             gt.stamp('evaluation sampling')
+            print("epoch = ", epoch)
+            for i in range(self.num_train_loops_per_epoch):
+                print( i,"-th train loop per epoch")
 
-            for _ in range(self.num_train_loops_per_epoch):
-                new_expl_paths = self.expl_data_collector.collect_new_paths(
-                    self.max_path_length,
-                    self.num_expl_steps_per_train_loop,
-                    discard_incomplete_paths=False,
-                )
-                gt.stamp('exploration sampling', unique=False)
-
-                self.replay_buffer.add_paths(new_expl_paths)
-                gt.stamp('data storing', unique=False)
+                # new_expl_paths = self.expl_data_collector.collect_new_paths(
+                #     self.max_path_length,
+                #     self.num_expl_steps_per_train_loop,
+                #     discard_incomplete_paths=False,
+                # )
+                # gt.stamp('exploration sampling', unique=False)
+                # self.replay_buffer.add_paths(new_expl_paths)
+                # gt.stamp('data storing', unique=False)
 
                 self.training_mode(True)
-                for _ in range(self.num_trains_per_train_loop):
+                for j in range(self.num_trains_per_train_loop):
+                    print(j," -th train per train loop")
+                    new_expl_paths = self.expl_data_collector.collect_new_paths(
+                        self.max_path_length,
+                        self.num_expl_steps_per_train_loop,
+                        discard_incomplete_paths=False,
+                    )
+                    gt.stamp('exploration sampling', unique=False)
+                    self.replay_buffer.add_paths(new_expl_paths)
                     train_data = self.replay_buffer.random_batch(
                         self.batch_size)
-                    self.trainer.train(train_data)
+                    self.trainer.train([train_data, new_expl_paths])
                 gt.stamp('training', unique=False)
                 self.training_mode(False)
 
