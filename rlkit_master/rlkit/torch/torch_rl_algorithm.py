0854d2b1b2e545214b5dec4791aae55c4be268fa
Maria Golitsyna <GolitsynaM@ru.ibm.com>
2020-05-01 12:00:07 +0300

    change TorchRlTrainer for entropy purposes

--- rlkit_master/rlkit/torch/torch_rl_algorithm.py
+++ rlkit_master/rlkit/torch/torch_rl_algorithm.py
@@ -36,8 +36,9 @@ class TorchTrainer(Trainer, metaclass=abc.ABCMeta):
 
     def train(self, np_batch):
         self._num_train_steps += 1
-        batch = np_to_pytorch_batch(np_batch)
-        self.train_from_torch(batch)
+        # batch = np_to_pytorch_batch(np_batch)
+        # self.train_from_torch(batch)
+        self.train_from_torch(np_batch)
 
     def get_diagnostics(self):
         return OrderedDict([
