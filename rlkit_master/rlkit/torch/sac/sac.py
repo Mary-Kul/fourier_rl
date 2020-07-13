86cee62d9893f929a500940d6f4acb06eb22067b
AlexanderGri <grishin.alexgri@yandex.ru>
2020-07-14 01:55:35 +0300

    Remove unnecessary import

--- rlkit_master/rlkit/torch/sac/sac.py
+++ rlkit_master/rlkit/torch/sac/sac.py
@@ -10,7 +10,7 @@ from rlkit.core.eval_util import create_stats_ordered_dict
 from rlkit.torch.torch_rl_algorithm import TorchTrainer
 
 import os
-import pandas as pd
+
 
 class SACTrainer(TorchTrainer):
     def __init__(
